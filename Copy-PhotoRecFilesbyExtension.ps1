bf07a7a41e81e3a785892e397f777c42b7c5b4c0
Luca Conte <l.conte@gmail.com>
2016-07-08 13:25:56 +0200

    Update Copy-PhotoRecFilesbyExtension.ps1
    Added some infos and License to .NOTES

--- Copy-PhotoRecFilesbyExtension.ps1
+++ Copy-PhotoRecFilesbyExtension.ps1
@@ -32,7 +32,38 @@ In this example only MP4 and AVI files will be copied from the source folder to 
 .LINK
 http://www.powershellacademy.it/scripts
 .NOTES
+Written by: Luca Conte
+
+Find me on:
+* Website:	http://lucaconte.it
+* Twitter:  http://twitter.com/desmox796
+* Memrise:  http://www.memrise.com/user/desmox/courses/teaching/
+* MyBlog:   http://desmox796.wordpress.com
+
 IMPORTANT: *** THIS SCRIPT IS PROVIDED WITHOUT WARRANTY, USE AT YOUR OWN RISK *** 
+
+License:
+The MIT License (MIT)
+
+Copyright (c) 2016 Luca Conte
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 #>
 
